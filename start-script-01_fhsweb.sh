35f0b0764f538374fbd2c3d2ab30890719df85d4
Mike Bickerton <mb@bicko.org.uk>
2019-11-14 19:40:15 +0000

    Update and rename start-script.sh to start-script-01_fhsweb.sh

--- start-script-01_fhsweb.sh
+++ start-script-01_fhsweb.sh
@@ -14,4 +14,4 @@ test ! -d raspi-prep && git clone https://github.com/Bicko/raspi-prep.git
 #Get latest yaml, etc
 cd raspi-prep && git pull
 
-ansible-playbook ~/raspi-prep/playbook.yml
+ansible-playbook ~/raspi-prep/playbook-01_fhsweb.yml
