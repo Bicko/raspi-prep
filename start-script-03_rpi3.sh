c196af840b6d389fa3117cb13b8424d712fba5d9
Mike Bickerton <mb@bicko.org.uk>
2019-12-08 09:21:24 +0000

    Create start-script-03_rpi3.sh

---
+++ start-script-03_rpi3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Running as root?
+if [ "$(id -u)" != "0" ] ; then
+  echo "Please run this as root." 1>&2
+  exit 1
+fi
+
+#Install Ansible, if not already installed
+test ! -f /usr/local/bin/ansible && apt -y install python-pip && pip install ansible
+
+#add-apt-repository -y ppa:ubuntu-pi-flavour-makers/ppa
+#apt-get update
+
+#Install git, if not already installed
+test ! -f /usr/bin/git && apt -y install git
+
+#Get the repo, if not already present
+cd ~
+test ! -d raspi-prep && git clone https://github.com/Bicko/raspi-prep.git
+
+#Get latest yaml, etc
+cd raspi-prep && git pull
+
+ansible-playbook ~/raspi-prep/playbook-03_rpi3-heating.yml
