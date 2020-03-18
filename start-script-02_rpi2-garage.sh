34c6bc474fd5881d4d6292cae4b77e1e09c234a4
Mike Bickerton <mb@bicko.org.uk>
2020-03-18 20:34:13 +0000

    Update start-script-02_rpi2-garage.sh

--- start-script-02_rpi2-garage.sh
+++ start-script-02_rpi2-garage.sh
@@ -10,7 +10,7 @@ fi
 apt update
 
 #Install Ansible, if not already installed
-test ! -f /usr/local/bin/ansible && apt -y install python-pip && pip install ansible
+test ! -f /usr/local/bin/ansible && apt -y install python3-pip && pip3 install ansible
 
 #add-apt-repository -y ppa:ubuntu-pi-flavour-makers/ppa
 #apt-get update
