a308c3531f33750d3eec3611c66a0ac8d6882001
Mike Bickerton <mb@bicko.org.uk>
2021-11-28 17:21:15 +0000

    Create start-script-11_rpiz2.sh

---
+++ start-script-11_rpiz2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Running as root?
+if [ "$(id -u)" != "0" ] ; then
+  echo "Please run this as root." 1>&2
+  exit 1
+fi
+
+# Must update repo list first!
+apt update
+
+#Install Ansible, if not already installed
+test ! -f /usr/local/bin/ansible && apt -y install python3-pip && pip3 install ansible
+
+apt-get update
+
+#Install git, if not already installed
+test ! -f /usr/bin/git && apt -y install git
+
+#Get the repo, if not already present
+cd ~
+test ! -d raspi-prep && git clone https://github.com/Bicko/raspi-prep.git
+
+#Get latest yaml, etc
+cd raspi-prep && git pull
+
+ansible-playbook ~/raspi-prep/playbook-10_rpiz2-sniff.yml
