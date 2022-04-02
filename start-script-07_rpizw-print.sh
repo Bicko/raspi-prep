42c228ce803109c377bee680dc3e19900268e2b2
Mike Bickerton <mb@bicko.org.uk>
2022-04-02 11:09:57 +0100

    Update start-script-07_rpizw-print.sh

--- start-script-07_rpizw-print.sh
+++ start-script-07_rpizw-print.sh
@@ -22,4 +22,4 @@ test ! -d raspi-prep && git clone https://github.com/Bicko/raspi-prep.git
 #Get latest yaml, etc
 cd raspi-prep && git pull
 
-ansible-playbook ~/raspi-prep/playbook-07_rpi1b-print.yml
+ansible-playbook ~/raspi-prep/playbook-07_rpizw-print.yml
